e6a894b6b971af0fc83d0d631b2414768b501592
GMosquera1 <genesismosquera@pursuit.org>
2018-10-09 14:12:51 -0400

    Initial Commit

---
+++ introToStrings/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  introToStrings
+//
+//  Created by Genesis Mosquera on 10/9/18.
+//  Copyright © 2018 Genesis Mosquera. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
